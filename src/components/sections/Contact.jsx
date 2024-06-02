ad05ec2a936255adfd693ff004a26486fcb3507a
Jake Garcia <bballwiz4@gmail.com>
2024-06-02 14:34:15 -0600

    Scaffolded out the contact page.

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiMail } from "react-icons/fi";
 
 import SectionTitle from '../../shared/text/SectionTitle';
 
@@ -7,6 +8,17 @@ const Contact = () => {
     return (
         <div id="contact" className="mt-24 px-2 flex flex-col">
             <SectionTitle title="Contact"/>
+            <div className="flex flex-row">
+                <div className=""></div>
+                <div className=""></div>
+            </div>
+            <div className="">
+                <div className=""></div>
+                <div className="">
+                    <div className=""></div>
+                    <div className=""></div>
+                </div>
+            </div>
         </div>
     )
 };
